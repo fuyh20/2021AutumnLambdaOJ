ce87eea22fc0cfc3235b4e2b1041576aec13b682
Fuyh <fuyh_20@163.com>
2021-10-07 12:44:49 +0800

    modify:recursion

--- 岩石的重量.cpp
+++ 岩石的重量.cpp
@@ -1,15 +1,15 @@
 #include <cstdio>
 
 int weights[30000], big[30000];
-int ans(int i, int j)
-{
-    if (j - i == 1)
-        return (weights[i] > weights[j] ? weights[i] : weights[j]);
-    int a = ans(i, j - 2) + weights[j];
-    int b = ans(i + 2, j) + weights[i];
-    int c = ans(i + 1, j - 1) + (weights[i] > weights[j] ? weights[i] : weights[j]);
-    return ((a > b ? a : b) > c ? (a > b ? a : b) : c);
-}
+// int ans(int i, int j)
+// {
+//     if (j - i == 1)
+//         return (weights[i] > weights[j] ? weights[i] : weights[j]);
+//     int a = ans(i, j - 2) + weights[j];
+//     int b = ans(i + 2, j) + weights[i];
+//     int c = ans(i + 1, j - 1) + (weights[i] > weights[j] ? weights[i] : weights[j]);
+//     return ((a > b ? a : b) > c ? (a > b ? a : b) : c);
+// }
 
 int main()
 {
